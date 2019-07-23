12b6748031f65886a22d6ac19f3a1940c6e54974
kalyan1111 <kalyankamati@gmail.com>
2019-07-23 14:10:55 +0530

    shdjh

---
+++ script.js
@@ -0,0 +1,6 @@
+var age=Number(prompt("what is u r age"));
+if(age==18){
+alert( "Congratulations on your first year of driving. Enjoy the ride!");}else if(age<18){
+    alert( "Sorry, you are too young to drive this car. Powering off");}else if(age>18){alert("Powering On. Enjoy the ride!");}else{
+        alert("type proper value of your age");
+    }
